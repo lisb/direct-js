956305e6950645e19c40a870904e98b2ba00b106
sho.kuroda <sho.kuroda@l-is-b.com>
2021-04-16 15:06:14 +0900

    fix: Use 'account' option

--- test/login.js
+++ test/login.js
@@ -11,11 +11,19 @@ const url = require("url");
 const endpoint = "wss://api.direct4b.com/albero-app-server/api";
 const dotenv = path.join(process.cwd(), ".env");
 
+const args = process.argv.slice(2);
+if (args.length === 0 || !args[0].includes(':')) {
+  console.error('"id:password" was not found in arguments');
+  process.exit(1);
+}
+const account = args[0];
+
 const client = direct.getInstance();
 
 client.setOptions({
   host: url.parse(endpoint).host,
-  endpoint: endpoint
+  endpoint: endpoint,
+  account
 });
 
 client.on("access_token_changed", token => {
